c6962a92e5634871ffc5f38af488af90ab074596
anandatswayam <anandconcious@gmail.com>
2016-10-19 13:00:27 +0530

    Carousal Cards Animation for Android

---
+++ app/src/main/java/swayam/carousel/MainActivity.java
@@ -0,0 +1,152 @@
+package swayam.carousel;
+
+import android.os.Build;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator;
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends FragmentActivity {
+
+    private TextView indicatorTv;
+    private View positionView;
+    private ViewPager viewPager;
+    private List<CommonFragment> fragments = new ArrayList<>();
+    private final String[] imageArray = {"assets://image1.png", "assets://image2.png", "assets://image3.png", "assets://image4.png", "assets://image5.png"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        positionView = findViewById(R.id.position_view);
+        dealStatusBar();
+
+        initImageLoader();
+
+        fillViewPager();
+    }
+
+    private void fillViewPager() {
+        indicatorTv = (TextView) findViewById(R.id.indicator_tv);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        viewPager.setPageTransformer(false, new CustPagerTransformer(this));
+
+        for (int i = 0; i < 5; i++) {
+            fragments.add(new CommonFragment());
+        }
+
+        viewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                CommonFragment fragment = fragments.get(position );
+                fragment.bindData(imageArray[position]);
+                return fragment;
+            }
+
+            @Override
+            public int getCount() {
+                return 5;
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                updateIndicatorTv();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        updateIndicatorTv();
+    }
+
+    private void updateIndicatorTv() {
+        int totalNum = viewPager.getAdapter().getCount();
+        int currentItem = viewPager.getCurrentItem() + 1;
+        indicatorTv.setText(Html.fromHtml("<font color='#12edf0'>" + currentItem + "</font>  /  " + totalNum));
+    }
+
+    private void dealStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            int statusBarHeight = getStatusBarHeight();
+            ViewGroup.LayoutParams lp = positionView.getLayoutParams();
+            lp.height = statusBarHeight;
+            positionView.setLayoutParams(lp);
+        }
+    }
+
+    private int getStatusBarHeight() {
+        Class<?> c = null;
+        Object obj = null;
+        Field field = null;
+        int x = 0, statusBarHeight = 0;
+        try {
+            c = Class.forName("com.android.internal.R$dimen");
+            obj = c.newInstance();
+            field = c.getField("status_bar_height");
+            x = Integer.parseInt(field.get(obj).toString());
+            statusBarHeight = getResources().getDimensionPixelSize(x);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        return statusBarHeight;
+    }
+
+    @SuppressWarnings("deprecation")
+    private void initImageLoader() {
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+                this)
+                .memoryCacheExtraOptions(480, 800)
+                .threadPoolSize(3)
+                .threadPriority(Thread.NORM_PRIORITY - 1)
+                .tasksProcessingOrder(QueueProcessingType.FIFO)
+                .denyCacheImageMultipleSizesInMemory()
+                .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                .memoryCacheSize(2 * 1024 * 1024).memoryCacheSizePercentage(13)
+                .discCacheSize(50 * 1024 * 1024)
+                .discCacheFileCount(100)
+                .discCacheFileNameGenerator(new HashCodeFileNameGenerator())
+                .imageDownloader(new BaseImageDownloader(this))
+                .defaultDisplayImageOptions(DisplayImageOptions.createSimple())
+                .writeDebugLogs().build();
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.init(config);
+    }
+
+}
